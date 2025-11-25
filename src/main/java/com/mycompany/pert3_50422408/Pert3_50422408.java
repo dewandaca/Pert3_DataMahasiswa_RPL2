66b342df7ab96227a2dc207a2fbe5e8007a0ee2f
dewandaca <121083307+dewandaca@users.noreply.github.com>
2025-11-25 16:22:47 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert3_50422408/Pert3_50422408.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_50422408;
+
+/**
+ *
+ * @author Acer
+ */
+public class Pert3_50422408 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
